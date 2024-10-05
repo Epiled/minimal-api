325425b03d3ae04d584221edb277bc9b42a8e777
Felipe De Andrade <felipe.deam98@gmail.com>
2024-10-05 07:54:15 -0300

    feat(project-setup): add initial libraries and setup folder architecture

--- Program.cs
+++ Program.cs
@@ -3,4 +3,14 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+app.MapPost("/login", (MinimalApi.DTOs loginDTO) => {
+if(loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456") {
+  return Results.Ok("Login com sucesso");
+} else {
+  return Results.Unauthorized();
+
+}
+});
+
 app.Run();
+
